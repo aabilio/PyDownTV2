badb1f25fca13e8bdb4d60b83bb9c7ae87759ed2
aabilio <aabilio@gmail.com>
2012-12-10 01:34:07 +0100

    Añadiendo plugins para navegadores (versiones antiguas)

---
+++ plugins/Firefox/doc/main.md
@@ -0,0 +1,2 @@
+The main module is a program that creates a widget.  When a user clicks on
+the widget, the program loads the pydowntv.com website in a new tab.
---
+++ plugins/Firefox/lib/main.js
@@ -0,0 +1,20 @@
+const widgets = require("widget");
+const tabs = require("tabs");
+
+var widget = widgets.Widget({
+  id: "pydowntv-link",
+  label: "Buscar en pydowntv",
+  contentURL: "http://web.pydowntv.com/static/favicon.ico",
+  onClick: function() {
+  	tabUrl = tabs.activeTab.url;
+    if (tabUrl.search("rtpa.es") != -1)
+    {
+      tabUrl = escape(tabUrl);
+    }
+    var url = "http://web.pydowntv.com/?url="+tabUrl;
+    
+    tabs.open(url);
+  }
+});
+
+console.log("The add-on is running.");
